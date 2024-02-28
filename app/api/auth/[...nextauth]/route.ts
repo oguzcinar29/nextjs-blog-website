6cfa815c76669222232203701bfa84ed8a3e39ba
Oguz Cinar <marazoguz_29@hotmail.com>
2024-02-28 16:45:07 +0300

    trying

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,30 @@
 import NextAuth from "next-auth/next";
-import { CredentialsProvider } from "next-auth/providers/credentials";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { Promise } from "es6-promise";
+import { signIn } from "next-auth/react";
+import { SessionStrategy } from "next-auth";
+
 const authOptions = {
-  providers: [CredentialsProvider({})],
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+
+      async authorize(credentials) {
+        const user = { id: "1" };
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt" as SessionStrategy,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login",
+  },
 };
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
