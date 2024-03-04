d7700e2ab205c113d5c359603116687eb9f243d7
Oguz Cinar <marazoguz_29@hotmail.com>
2024-03-04 11:54:37 +0300

    added no store to cache

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -27,7 +27,7 @@ export default function Login() {
   const getAllUsers = async () => {
     try {
       const data = await fetch(`${apiURL}/api/user`, {
-        cache: "no-cache",
+        cache: "no-store",
       });
       if (!data.ok) {
         throw new Error("Failed to fetch posts");
